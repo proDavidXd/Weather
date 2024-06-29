6a692063d7882bf0c1b8725da0c3c8284abf3115
David <daviaaljaque@gmail.com>
2024-06-29 11:32:51 -0600

    search

--- src/data.ts
+++
@@ -1,7 +0,0 @@
-export default async function getData(city: string) {
-  const res = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=8e024bb739704c6281d182243232312&q=${city}&aqi=no`
-  );
-  const data = await res.json();
-  return data;
-}
